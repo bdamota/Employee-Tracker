cce065c4175051f55d8958423d53cf436835b28c
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-07 16:11:16 -0700

    tables created in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,26 @@
-DROP TABLE IF EXISTS employee-tracker;
+DROP TABLE IF EXISTS employee_tracker;
 
 CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 
 CREATE TABLE department (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-  id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL (10.0), 
-  department_id INTEGER PRIMARY KEY, 
+  department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employees (
+  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL, 
+  CONSTRAINT fk_party FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+  manager_id INTEGER 
 );
