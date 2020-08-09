903d33d82a95dc462ece142ddfd6466bc4a1e831
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-08 21:52:04 -0700

    method added to add a new department

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ function initialOptions() {
     //query to view all departments
     const query = connection.query("SELECT * FROM department", function (err, res) {
         if (err) throw err
-        console.log("All Deparments. \n");
+        console.log("All Deparments: \n");
         console.table(res);
         initialOptions();
     });
@@ -74,11 +74,42 @@ function initialOptions() {
     //query to view roles with department ID returned with name
     const query = connection.query("SELECT roles.id, roles.title, roles.salary, dept_name as department FROM roles JOIN department ON roles.department_id = department.id", function (err, res) {
         if (err) throw err
-        console.log("Roles. \n")
+        console.log("Employee Roles: \n")
         console.table(res);
         initialOptions();
     });
   };
+
+  function viewAllEmployees() {
+    const query = connection.query("SELECT e1.id, e1.first_name, e1.last_name, roles.title as role, dept_name AS department, roles.salary, Concat(e2.first_name, ' ', e2.last_name) AS manager FROM employees e1 LEFT JOIN roles ON e1.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employees e2 ON e2.id = e1.manager_id", function (err, res) {
+        if (err) throw err
+        console.log ("\n All Employee Info: \n");
+        console.table(res);
+        initialOptions();
+    });
+  };
+
+  function addDepartment() {
+    inquirer.prompt ({
+        type: 'input',
+        name: 'department',
+        message: 'What is the name of the new department?'
+    }).then(function(input) {
+        const query = connection.query(
+          'INSERT INTO department SET ?',
+          {
+            dept_name: input.department
+          },
+          function(err, res) {
+            if (err) throw err;
+            console.log('Department added!\n');
+            initialOptions();
+          });
+    });
+    
+  };
+  
+  
 
   initialOptions();
   
--- db/seeds.sql
+++ db/seeds.sql
@@ -9,13 +9,21 @@ VALUES
 INSERT INTO roles (title, salary, department_id)
 VALUES
   ('HR Director', 90000, 1),
+  ('Recruiter', 50000, 1),
   ('VP of Finance', 150000, 2),
+  ('Finance Manager', 65000, 2),
+  ('Head of Engineering', 300000, 3),
   ('Software Engineer', 100000, 3),
-  ('Head of Design', 200000, 4);
+  ('Head of Design', 200000, 4),
+  ('Product Manager', 80000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-  ('John', 'Smith', 1, 1),
-  ('Jane', 'Doe', 2, 3),
-  ('Scott', 'Dean', 3, NULL),
-  ('Sarah', 'Reed', 4, 5);
+  ('John', 'Smith', 1, NULL),
+  ('Carl', 'Phillips', 2, 1),
+  ('Jane', 'Doe', 3, NULL),
+  ('Shelly', 'Crane', 4, 3),
+  ('Scott', 'Dean', 5, NULL),
+  ('Shawn', 'Johnson', 6, 5),
+  ('Sarah', 'Reed', 7, NULL),
+  ('Jake', 'Jones', 8, 7);
