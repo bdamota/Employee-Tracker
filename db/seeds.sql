fd40bf628fa345506082f62129ad45474da44f6a
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-08 20:41:24 -0700

    tables manually added

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,10 +8,10 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-  ('HR Director', 90,000, 1),
-  ('VP of Finance', 150,000, 2),
-  ('Software Engineer', 100,000, 3),
-  ('Head of Design', 200,000, 4);
+  ('HR Director', 90000, 1),
+  ('VP of Finance', 150000, 2),
+  ('Software Engineer', 100000, 3),
+  ('Head of Design', 200000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
